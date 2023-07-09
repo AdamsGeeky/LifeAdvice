807671050611173eee5dd1eed1f0f68cd5fa9e4b
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2023-07-10 00:07:31 +0100

    feat: Good

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [advice, setAdvice] = useState()
+
+    async function getAdvice(){
+      const res = await fetch("https://api.adviceslip.com/advice");
+      const data = await res.json();
+      setAdvice(data.slip.advice);
+      setCount((prev) => prev + 1);
+    } 
+
+    useEffect(function(){
+      getAdvice()
+    },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <span>Advice of tha day</span>
+      <h1>{advice}</h1>
+
+     <button onClick={getAdvice}>Get Advice</button>
+
+     <p> You have read <strong>{count}</strong></p>
+    </div>
   )
 }
 
